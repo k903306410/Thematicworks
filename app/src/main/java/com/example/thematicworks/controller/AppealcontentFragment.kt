f5fc4037eb53eaa7bfd45d9bcdf60fa1797279da
AllenSuX <klovefly520@yahoo.com.tw>
2023-09-12 13:57:24 +0800

    修一下Sinpper

--- app/src/main/java/com/example/thematicworks/controller/AppealcontentFragment.kt
+++ app/src/main/java/com/example/thematicworks/controller/AppealcontentFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
@@ -38,21 +39,41 @@ class AppealcontentFragment : Fragment() {
            }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-with(binding){
+with(binding) {
+    val onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ) {
+
+            val selectedValue = parent?.getItemAtPosition(position).toString()
+            viewModel?.appeal?.value?.regulations = selectedValue
+
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+
+formmenu.setSelection(0,true)
+            formmenu.onItemSelectedListener=onItemSelectedListener
+
+
 goout.setOnClickListener {
-//    val list = viewModel.menu.value
+
 //    val APPeal = viewModel.appeal.value
 //    val gson = Gson()
 //    var jsonObject = JsonObject()
-//    jsonObject.addProperty("menu", gson.toJson(list))
 //    jsonObject.addProperty("appeal", gson.toJson(APPeal))
 //    val respBody = requestTask<JsonObject>(urlFineAppeal, "POST", jsonObject.toString())
-  //  respBody?.run {
-    //    if (get("successful").asBoolean) {
+//    respBody?.run {
+//        if (get("successful").asBoolean) {
             Navigation.findNavController(it).navigate(R.id.appealFragment)
             Toast.makeText(requireContext(), "申訴成功", Toast.LENGTH_SHORT).show()
       //  }
-    //}我把上傳到Server端的部分註解掉,以方便直接執行
+//    }我把上傳到Server端的部分註解掉,以方便直接執行
 
 }
 }
--- app/src/main/java/com/example/thematicworks/model/Appeal.kt
+++ app/src/main/java/com/example/thematicworks/model/Appeal.kt
@@ -3,7 +3,7 @@ package com.example.thematicworks.model
 import java.io.Serializable
 
 data class Appeal (
-
+var regulations : String="",
     var remark:String="",//h 申訴理由2 a
 
 ): Serializable
